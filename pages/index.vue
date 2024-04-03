869b180c115308bc7a69b3a004ad409a039fe18c
MassimoAurelio <massimoaureliowork@gmail.com>
2024-04-03 16:58:00 +0300

    add eslint

--- pages/index.vue
+++ pages/index.vue
@@ -17,5 +17,16 @@ console.log(colorMode.preference);
 </template>
 
 <style lang="scss">
-
+body {
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.8);
+}
+.dark-mode body {
+  background-color: #0f0f0f;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
 </style>
