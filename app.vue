f2eb5b4ee2f7889ba5a1f6f78fb561ecd4763966
MassimoAurelio <massimoaureliowork@gmail.com>
2024-04-03 16:28:30 +0300

    ( ° ʖ °)

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style scoped lang="scss">
+body {
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.8);
+}
+.dark-mode body {
+  background-color: #0f0f0f;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@vueuse/nuxt", "@nuxt/content", "@nuxtjs/color-mode"],
+  css: ["@/assets/scss/global.scss"],
+  colorMode: {
+    preference: "system", 
+    fallback: "light", 
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "-mode",
+    storageKey: "nuxt-color-mode",
+  },
+});
